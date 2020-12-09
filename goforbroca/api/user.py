6ebd528da6fcede893451cf96b06a790d63c3189
Catherine Lin <catherine.lin.93@gmail.com>
2020-12-08 19:19:50 -0800

    added sms enabled and phone number field and api

--- goforbroca/api/user.py
+++ goforbroca/api/user.py
@@ -28,7 +28,7 @@ def post(google_id: str) -> Response:
     if user is not None:
         return make_response({'msg': 'user already exists'}, 400)
 
-    user = User.create(email_address=email_address, google_id=google_id)
+    user = User.create(email_address=email_address, google_id=google_id,sms_enabled=False)
     user_deck = UserDeck.create_default_deck(user.id)
     return make_response({'user': user_schema.dump(user).data, 'user_deck': user_deck_schema.dump(user_deck).data}, 200)
 
@@ -42,4 +42,16 @@ def get(user: User) -> Response:
 @user_blueprint.route('/', methods=['PUT'])
 @wrap_authenticated_user
 def put(user: User) -> Response:
-    raise NotImplementedError()
+    sms_enabled = request.json.get('sms_enabled')
+    phone_number = request.json.get('phone_number')
+
+    user = User.query.filter_by(id=user.id).scalar()
+
+    if sms_enabled is not None:    
+        user.sms_enabled = sms_enabled
+    if phone_number is not None:
+        user.phone_number = phone_number
+    
+    user.save()
+
+    return make_response({'user': user_schema.dump(user).data}, 200)
--- goforbroca/models/user.py
+++ goforbroca/models/user.py
@@ -8,3 +8,5 @@ class User(Base):
     email_address = db.Column(db.String(256), unique=True, nullable=False, index=True)
     google_id = db.Column(db.String(64), unique=True, nullable=True, index=True)
     facebook_id = db.Column(db.String(64), unique=True, nullable=True, index=True)
+    sms_enabled = db.Column(db.Boolean(False), index=True)
+    phone_number = db.Column(db.String(10), unique=True, index=True)
